ba19246bfa1db7bd318bc947313494f7adf749fb
Gjon Gojcevic <gjonn@hotmail.com>
2023-04-07 13:15:21 -0400

    Made SSO automatic if not logged in. Sign out button is set to redirect

--- FrontendCode/src/components/SignInButton.jsx
+++ FrontendCode/src/components/SignInButton.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
 
 /**
  * Renders a drop down button with child buttons for logging in with a popup or redirect
@@ -24,18 +23,6 @@ export const SignInButton = () => {
         }
     };
     return (
-        <DropdownButton
-            variant="secondary"
-            className="ml-auto"
-            drop="start"
-            title="Sign In"
-        >
-            <Dropdown.Item as="button" onClick={() => handleLogin("popup")}>
-                Sign in using Popup
-            </Dropdown.Item>
-            <Dropdown.Item as="button" onClick={() => handleLogin("redirect")}>
-                Sign in using Redirect
-            </Dropdown.Item>
-        </DropdownButton>
+        <Button variant="secondary" className="ml-auto" drop="start" onClick={() => handleLogin("redirect")}>Sign In</Button>
     );
 };
