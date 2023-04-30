8c61d8b6c40cc097d05c73758c835aa0110fdd20
Brandon Casey <bcasey266@gmail.com>
2023-04-30 13:06:44 -0500

    Improved Output Wording

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,9 +1,9 @@
-output "api_admin_consent" {
+output "spn_api" {
   description = "Manual step required on the first deploy of the platform"
-  value       = module.APIM.api_admin_consent
+  value       = "Azure AD App Registration API Permissions: ${module.APIM.api_admin_consent}"
 }
 
-output "Office365Authorization" {
+output "o365_auth" {
   description = "Manual step required on the first deploy of the platform"
-  value       = module.Notifications.authorize
+  value       = "Office 365 Authorization: ${module.Notifications.authorize}"
 }
