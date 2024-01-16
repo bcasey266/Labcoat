b88c6ba15d90c586fdfb741e23589a0b49a500a4
Brandon Casey <bcasey266@gmail.com>
2024-01-15 21:18:22 -0600

    fix: bumped provider versions

--- Infra/versions.tf
+++ Infra/versions.tf
@@ -1,21 +1,21 @@
 terraform {
   required_version = ">= 1.4.0, <= 1.5.5"
 
-  #backend "azurerm" {
-  #}
+  backend "azurerm" {
+  }
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.80.0"
+      version = "~>3.87.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.45.0"
+      version = "2.47.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.9.0"
+      version = "1.10.0"
     }
   }
 }
