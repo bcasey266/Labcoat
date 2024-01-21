f0cb952c2cced941ec7ba39bd26d767c969f4163
Brandon Casey <bcasey266@gmail.com>
2024-01-21 15:34:21 -0600

    fix: azurerm provider feature improvement

--- Infra/providers.tf
+++ Infra/providers.tf
@@ -1,7 +1,13 @@
 provider "azurerm" {
   features {
+    application_insights {
+      disable_generated_rule = false
+    }
     key_vault {
       recover_soft_deleted_secrets = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
   }
   storage_use_azuread = true
